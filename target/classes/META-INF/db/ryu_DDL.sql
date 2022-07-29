8403d110261885c321890e91a981d838910415cf
naradin711 <hajime711@naver.com>
2022-07-29 13:43:53 +0900

    ryuAddScoreAfterMerge

--- target/classes/META-INF/db/ryu_DDL.sql
+++ target/classes/META-INF/db/ryu_DDL.sql
@@ -1,96 +1,76 @@
 -- ryu ddl store / storere /request table create and drop
 drop table store;
-
 drop SEQUENCE store_sq;
-drop table store;
-create SEQUENCE store_sq MAXVALUE 999999 NOCACHE NOCYCLE; 
+create SEQUENCE store_sq MAXVALUE 999999 NOCACHE NOCYCLE;
 create table store (
-            sNum       number(8) PRIMARY KEY, 
-            sName       varchar2(100) not null, 
-            sAddress    varchar2(250) not null, 
-            sTel        varchar2(100) not null, 
-            sType       varchar2(100) not null, 
-            sPrice      varchar2(100) not null, 
-            sTime       varchar2(100) not null, 
-            sHoliday    varchar2(100) not null, 
-            sMenu1      varchar2(100) not null, 
-            sMenu1cost  varchar2(100) not null, 
-            sMenu2      varchar2(100), 
-            sMenu2cost  varchar2(100), 
-            sMenu3      varchar2(100), 
-            sMenu3cost  varchar2(100),
-            sMenu4      varchar2(100),
-            sMenu4cost  varchar2(100),
-            sMenu5      varchar2(100),
-            sMenu5cost  varchar2(100),
-            sSearchtag  varchar2(100),
-            sConfirm    char(1) default 'N',
-            sReplycnt   number(8) default 0,
-            sScore      number(8) default 0,
-            sLike number(1) default 0 
+            sNo        number(8) PRIMARY KEY,
+            oId         varchar2(50),
+            sImage      varchar2(250) not null,
+            sName       varchar2(100) not null,
+            sAddress    varchar2(250) not null,
+            sTel        varchar2(100) not null,
+            sType       varchar2(100) not null,
+            sPrice      varchar2(100) not null,
+            sTime       varchar2(100) not null,
+            sHoliday    varchar2(100) not null,
+            sMenu1      varchar2(100) not null,
+            sMenu1cost  varchar2(100) not null,
+            sMenu2      varchar2(100),
+            sMenu2cost  varchar2(100),
+            sMenu3      varchar2(100),
+            sMenu3cost  varchar2(100),
+            sMenu4      varchar2(100),
+            sMenu4cost  varchar2(100),
+            sMenu5      varchar2(100),
+            sMenu5cost  varchar2(100),
+            sSearchtag       varchar2(100),
+            sConfirm    char(1) default 'N',
+            sReplycnt   number(8) default 0,
+            sScore      number(8) default 0,
+            sLike       number(1) default 0
             );
-
-SELECT * FROM STORE;
-
-SELECT STORENAME, STORETYPE, (sScore/sReplycnt)STSCORE FROM STORE; -- 가게 평점
-
-    
-
-    
-
-insert into store (sNum, sImage, sName, sAddress, sTel, sType, sPrice, sTime, sHoliday,
-
-            sMenu1 , sMenu1cost , sMenu2, sMenu2cost, sMenu3, sMenu3cost, sMenu4, sMenu4cost,
-
-            sMenu5, sMenu5cost, sSearchtag) 
-
-values (store_sq.nextval, 'noimg.jpg', '군옥각', '리월시 취헌부', '070-8888-8888', '중식, 중국요리, 짜장면', '2만원-4만원대',  
-
-        '11:00-19:00', '금요일', '짜장면', '5000원', '짬뽕', '6000원', '탕수육', '10000원', '간짜장', '7500원', '굴짬뽕', '9000원',
-
-        '#강남#강남역#강남역맛집#중국집#중화요리#중국요리');
-
-        
-
-UPDATE STORE SET   storereplycnt = storereplycnt +1 ,
-
-                   storescore = storescore +2
-
-                   WHERE STORENUM = 1;
-
-                   
-
+SELECT * FROM STORE; 
+SELECT SNAME, STYPE, (sScore/sReplycnt)STSCORE FROM STORE where sNo = 1; -- 가게 평점
+    
+    
+insert into store (sNo, oId, sImage, sName, sAddress, sTel, sType, sPrice, sTime, sHoliday,
+            sMenu1 , sMenu1cost , sMenu2, sMenu2cost, sMenu3, sMenu3cost, sMenu4, sMenu4cost,
+            sMenu5, sMenu5cost, sSearchtag) 
+values (store_sq.nextval, 'aaa', 'noimg.jpg', '군옥각', '리월시 취헌부', '070-8888-8888', '중식, 중국요리, 짜장면', '2만원-4만원대',  
+        '11:00-19:00', '금요일', '짜장면', '5000원', '짬뽕', '6000원', '탕수육', '10000원', '간짜장', '7500원', '굴짬뽕', '9000원',
+        '#강남#강남역#강남역맛집#중국집#중화요리#중국요리');
+commit;        
+        
+UPDATE STORE SET   storereplycnt = storereplycnt +1 ,
+                   storescore = storescore +2
+                   WHERE STORENUM = 1;
+                   
 drop table storereview;
-
 drop SEQUENCE storereview_sq;
-
 create SEQUENCE storereview_sq MAXVALUE 999999 NOCACHE NOCYCLE;
-
 create table storereview (
-
-            reviewnum         number(8) PRIMARY KEY,
-
-            storenum          number(8) REFERENCES store(storenum),
-
-            userprofile       varchar2(1000) not null,
-
-            userid            varchar2(100),
-
-            reviewcontent     varchar2(2000) not null,
-
-            reviewimage1      varchar2(500),
-
-            reviewimage2      varchar2(500),
-
-            reviewimage3      varchar2(500),
-
-            reviewimage4      varchar2(500),
-
-            reviewimage5      varchar2(500),
-
-            reviewscore       number(3) default 0,
-
-            reviewdate        date default sysdate
-
-    );    
-
+            srNo         number(8) PRIMARY KEY,
+            sNo          number(8) REFERENCES store(sNo),
+            mProfile       varchar2(1000) not null,
+            mId            varchar2(100),
+            srContent     varchar2(2000) not null,
+            srImage1      varchar2(500),
+            srImage2      varchar2(500),
+            srImage3      varchar2(500),
+            srImage4      varchar2(500),
+            srImage5      varchar2(500),
+            srScore       number(3) default 0,
+            srDate        date default sysdate
+    );  
+    
+drop table request;
+drop SEQUENCE request_sq;
+create SEQUENCE request_sq MAXVALUE 999999 NOCACHE NOCYCLE;
+create table request (
+            rNo           number(8) PRIMARY KEY,--  요청 글 번호
+            sNo           number(8) REFERENCES store(sNo), -- 요청한 가게 번호
+            oid           varchar2(50) not null, -- 요청한 사장님 아이디
+            aId           varchar2(50), -- 관리자 아이디
+            sName       varchar2(100) not null, -- 요청한 가게 이름
+            rDate        date default sysdate -- 요청 등록된 일시
+    );    
--- target/classes/META-INF/db/ryu_DML.sql
+++ target/classes/META-INF/db/ryu_DML.sql
@@ -1,1 +1,73 @@
--- ryu DML store / request queries
+-- ryu DML store / storereview / request queries
+
+-- table store
+-- 1. storeRegister / 가게 등록하기
+insert into store (sNo, oId, sImage, sName, sAddress, sTel, sType, sPrice, sTime, sHoliday,
+            sMenu1 , sMenu1cost , sMenu2, sMenu2cost, sMenu3, sMenu3cost, sMenu4, sMenu4cost,
+            sMenu5, sMenu5cost, sSearchtag) 
+values (store_sq.nextval, 'chinachef', 'noimg.jpg', '군옥각', '리월시 취헌부', '070-8888-8888', '중식, 중국요리, 짜장면', '2만원-4만원대',  
+        '11:00-19:00', '금요일', '짜장면', '5000원', '짬뽕', '6000원', '탕수육', '10000원', '간짜장', '7500원', '굴짬뽕', '9000원',
+        '#강남#강남역#강남역맛집#중국집#중화요리#중국요리');
+-- 2. storeModify / 가게 정보 수정하기
+update store set sImage = 'modify.jpg',
+                 sName = '청진각',
+                 sAddress = '리월시 칠성부 옥형성면',
+                 sTel = '010-9998-8888',
+                 sType = '중식', 
+                 sPrice = '1만원-2만원대',
+                 sTime = '24시간' ,
+                 sHoliday = '설날, 추석, 공휴일',
+                 sMenu1 = '황금새우볼튀김',
+                 sMenu1cost = '15000원',
+                 sMenu2 = '리월두부',
+                 sMenu2cost = '11500원',
+                 sMenu3 = '용수면',
+                 sMenu3cost = '9500원',
+                 sMenu4 = '마라전골',
+                 sMenu4cost = '13500원',
+                 sMenu5 = '훠궈',
+                 sMenu5cost = '13400원',
+                 sSearchtag = '#리월#리월맛집#옥형성#각청'
+                 where sNo = 2 and oId = 'aaa'; 
+                 
+-- 3. storeDetail / 가게 정보 상세보기 (sNo로 DTO 불러오기)
+SELECT * FROM STORE WHERE SNO = 1;
+
+
+-- 4. storeDelete / 가게 정보 삭제 (해당 가게 사장님 아이디 / 관리자 사용 가능)
+DELETE Store where sNo = 2 and oId = 'aaa';
+
+-- 5. 해당 가게 평점 출력하기 (메인 페이지에 이와 같이 출력)
+                 
+SELECT SNAME, STYPE, (sScore/sReplycnt)STSCORE FROM STORE WHERE SNO = 1;
+
+
+-- table storereview
+
+-- 1. reviewWrite / 가게의 리뷰 쓰기
+insert into storereview (srNo, sNo, mProfile, mId, srContent, 
+    srImage1, srImage2, srImage3,  srImage4, srImage5, srScore, srDate )
+VALUES (storereview_sq.NEXTVAL, 1, 'noImg.png', 'aaa', '여기 너무 맛있어요',
+        'noImg.png', 'noImg.png', 'noImg.png', 'noImg.png', 'noImg.png',
+         4 , sysdate);
+         
+-- 1. - 1. addScore / 가게의 평점 등록 (reviewWrite와 동시에 이뤄지며 score + 숫자에는 srScore 가 들어갈 예정)         
+UPDATE STORE SET   sReplycnt = sReplycnt +1 ,
+                   sScore = sScore + 4
+                   WHERE SNO = 1;
+         
+-- table request
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
